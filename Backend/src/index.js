f71d9679f38fbf1e0832793b0b13fe32f0dd0560
Ansik Singh Tomar <aanshiksinghtomar@gmail.com>
2026-06-28 21:37:16 +0530

    Edited the index file to add new routes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,25 +1,39 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
-import dbConnect from "./config/dbConnect.js";
-import authRoutes from "./routes/authRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
+import farmerGroupRoutes from "./routes/farmerGroupRoutes.js";
+import collectionRoutes from "./routes/collectionRoutes.js";
 
 dotenv.config();
-dbConnect();
-
-
 const app = express();
 
-// Middlewares
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+}));
+
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api/groups", farmerGroupRoutes);
+app.use("/api/collections", collectionRoutes);
 
-// Server
+app.get("/", (req, res) => {
+  res.json({ message: "FarmFresh API is running" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal server error" });
+});
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-  console.log(`Server is running at port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
