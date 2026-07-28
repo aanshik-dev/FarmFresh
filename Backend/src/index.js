903c20b8bf7954be0531538a81c5b101f6d41c11
Ansik Singh Tomar <aanshiksinghtomar@gmail.com>
2026-07-28 23:00:56 +0530

    added scripts for hosting on render

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL, "http://localhost:5173"] : "http://localhost:5173",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   }),
